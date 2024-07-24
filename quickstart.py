a3961e9044d66225368fc71d7f3ac65d4ec0c599
Yangkun Ai <yangkun.ayk@gmail.com>
2024-07-24 15:21:23 +0800

    Initial commit

---
+++ quickstart.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np
+
+X_train = np.array([1.0, 2.0, 3.0, 4.0])
+y_train = np.array([5.0, 8.0, 11.0, 14.0])
+
+# define the model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(1, input_shape=[1])
+])
+
+# compile the model, loss function and optimizer.
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+# traint
+model.fit(X_train, y_train, epochs=500)
+
+# predit
+X_test = np.array([5.0, 6.0, 7.0])
+y_pred = model.predict(X_test)
+
+print(y_pred)
